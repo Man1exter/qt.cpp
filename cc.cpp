75879a6aff1306fafc896e1bfe2523d866c549c4
Mariusz Perzynski <m.perzyk@interia.pl>
2021-01-30 13:57:59 +0100

    only delete space

--- cc.cpp
+++ cc.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 int main(){
 
 int i;
@@ -12,6 +11,5 @@ for(int i = 0; i < 32; i++){
          << 64 + i << " (" << char(64 + i) << ") "
          << 96 + i << " (" << char(96 + i) << ") " << endl;
 }
-
 return 0;
 }
